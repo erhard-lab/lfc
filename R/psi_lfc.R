283a386af6e98bd6019ba6bbed14ecdb31de7dee
Florian Erhard <flo@erhard-mail.de>
2017-11-03 16:25:51 +0100

    Doku

--- R/psi_lfc.R
+++ R/psi_lfc.R
@@ -31,19 +31,84 @@ EmpiricalBayesPrior=function(A,B) {
 #' @return A vector of length 2 containing the two parameters
 #' @export
 #' @importFrom stats median
-CenterMedian <- function(l) l-median(l)
+CenterMedian <- function(l) l-median(l,na.rm=T)
 
-#' Computes the optimal effect size estimate.
+#' Computes the optimal effect size estimate and credible intervals if needed.
 #'
+#' @title Psi LFC effect size estimator
 #' @param A Vector of counts from condition A
 #' @param B Vector of counts from condition B
 #' @param prior Vector of length 2 of the prior parameters
 #' @param normalizeFun Function to normalize the obtained effect sizes
+#' @param cre Compute credible intervals as well? (can also be a vector of quantiles)
 #'
-#' @return A vector of length 2 containing the two parameters
+#' @return Either a vector containing the estimates, or a data frame containing
+#'     the credible interval as well
 #' @export
-PsiLFC=function(A,B, prior=EmpiricalBayesPrior(A,B), normalizeFun=CenterMedian) {
-    lfc=(digamma(A+prior[1])-digamma(B+prior[2]))/log(2)
-    CenterMedian(lfc)
+PsiLFC=function(A,B, prior=EmpiricalBayesPrior(A,B), normalizeFun=CenterMedian,cre=FALSE) {
+    lfc<-(digamma(A+prior[1])-digamma(B+prior[2]))/log(2)
+    r<-CenterMedian(lfc)
+    if (all(cre==T)) cre = c(0.05,0.95)
+    if (!missing(cre) & any(cre!=FALSE)){
+        s<-sapply(cre,function(c) qlfc(c,A+prior[1],B+prior[2]))
+        r<-cbind(data.frame(PsiLFC=lfc),s)
+        names(r)[-1]<-paste("Credible",cre)
+    }
+    r
 }
+
+
+#' Computes the optimal effect size estimate and credible intervals if needed
+#' for a Bioconductor SummarizedExperiment object
+#'
+#' @title Psi LFC effect size estimator
+#' @param se SummarizedExperiment object
+#' @param contrast Vector of length 3 (<name>,<A>,<B>)
+#' @param cre Compute credible intervals as well? (can also be a vector of quantiles)
+#'
+#' @return Either a vector containing the estimates, or a data frame containing
+#'     the credible interval as well
+#' @export
+PsiLFC.se=function(se,contrast,cre=F) {
+    if (!is.character(contrast) | length(contrast)!=3 | !(contrast[1] %in% names(colData(se))) | contrast[2]==contrast[3] ) {
+        stop("'contrast' vector should be a character vector of length (colData,A,B)")
+    }
+
+    f=colData(se)[,contrast[1]]
+    if (!all(contrast[2:3] %in% levels(f))){
+        stop("'contrast' vector contains unknown levels!")
+    }
+
+    summ<-function(cont) {
+        r=assay(se)[,f==cont]
+        if (!is.vector(r)) r=apply(r,1,sum)
+        r
+    }
+    A=summ(contrast[2])
+    B=summ(contrast[3])
+    PsiLFC(A,B,cre=cre)
+}
+
+
+#' Drop-in replacement for DESeq2's results function for simple settings involving
+#' a single variable. Appends the PsiLFC estimate.
+#'
+#' @title Psi LFC effect size estimator for DESeq2
+#' @param object the DESeq2DataSet object
+#' @param contrast Vector of length 3, specifying the variable and the two levels
+#'     to compute effect sizes for (<name>,<A>,<B>)
+#' @param cre Compute credible intervals as well? (can also be a vector of quantiles)
+#'
+#' @return Either a vector containing the estimates, or a data frame containing
+#'     the credible interval as well
+#' @export
+results <- function(object, contrast, cre=F, ...) {
+    r=PsiLFC.se(object,contrast,cre)
+    if ("DESeq2" %in% loadedNamespaces()) {
+        if (is.vector(r)) r = data.frame(PsiLFC=r)
+        r=cbind(DESeq2::results(object,contrast,...),r)
+    }
+    r
+}
+
 
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # lfc
 R package for modelling count ratio data
+
+## How to get started
+
+Install the R package using the following commands on the R console:
+
+```
+install.packages("devtools")
+devtools::install_github("florianerhard/lfc")
+library(lfc)
+```
+
+
+## Brief introduction
+
+Digital expression measurements (e.g. RNA-seq) are often used to determine the change of quantities upon some treatment or stimulus. The resulting value of interest is the *fold change* (often logarithmized).
+
+This effect size of the change is often treated as a value that can be computed as $lfc(A,B)=\log_2 \frac{A}{B}$. However, due to the probabilistic nature of the experiments, the effect size rather is a random variable that must be estimated. This fact becomes obvious when considering that $A$ or $B$ can be 0, even if the true abundance is non-zero.
+
+We have shown that this can be modelled in a Bayesian framework (Paper at NAR)[https://dx.doi.org/10.1093%2Fnar%2Fgkv696]. The intuitively computed effect size is the maximum likelihood estimator of a binomial model, where the effect size is not represented as fold change, but as proportion (of note, the log fold change simply is the logit transformed proportion). The Bayesian prior corresponds to pseudocounts frequently used to prevent infinite fold changes by $A$ or $B$ being zero. Furthermore, the Bayesian framework offers more advanced estimators (e.g. interval estimators or the posterior mean, which is the optimal estimator in terms of squared errors).
+
+This R package offers the implementation to harness the power of this framework.
+
